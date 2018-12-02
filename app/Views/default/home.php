8ba60a50b3d860a8d22b8a4e4e8ee838635d6bf8
n-peugnet <n.peugnet@free.fr>
2018-12-02 19:24:27 +0100

    feat: better looking bithub button

--- app/Views/default/home.php
+++ app/Views/default/home.php
@@ -6,40 +6,46 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.6.0/p5.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.6.0/addons/p5.dom.min.js"></script>
 	<script src=<?= $this->assetUrl("js/script.js") ?> defer></script>
+	<script src="https://buttons.github.io/buttons.js" async defer></script>
 <?php $this->stop('js') ?>
 
 <?php $this->start('main_content') ?>
-	<div id="div-1" style="position: relative;"></div>
 
-	<iframe src="https://ghbtns.com/github-btn.html?user=n-peugnet&repo=image-map-creator&type=star&count=true" frameborder="0" scrolling="0" width="170px" height="20px"></iframe>
-	<form
-		action="<?= $this->url('api_insert_maps') ?>"
-		method="post"
-		id="publish-form"
-		enctype="multipart/form-data"
-	>
-		<label class="text">
-			pseudo
-			<input
-				type="text"
-				name="pseudo"
-				placeholder="anonymous"
-				minlength="2"
-				maxlength="32"
-				required
-			/>
-		</label>
-		<input type="submit" value="publish" id="publish-form-submit"/>
-		<img class="info hidden loading" src="<?= $this->assetUrl('img/loading.gif') ?>">
-		<img class="info hidden success" src="<?= $this->assetUrl('img/success.svg') ?>">
-		<img class="info hidden error" src="<?= $this->assetUrl('img/error.svg') ?>">
-	</form>
+	<div id="imc">
+		<div id="div-1" style="position: relative;"></div>
+
+		<form
+			action="<?= $this->url('api_insert_maps') ?>"
+			method="post"
+			id="publish-form"
+			enctype="multipart/form-data"
+		>
+			<label class="text">
+				pseudo
+				<input
+					type="text"
+					name="pseudo"
+					placeholder="anonymous"
+					minlength="2"
+					maxlength="32"
+					required
+				/>
+			</label>
+			<input type="submit" value="publish" id="publish-form-submit"/>
+			<img class="info hidden loading" src="<?= $this->assetUrl('img/loading.gif') ?>">
+			<img class="info hidden success" src="<?= $this->assetUrl('img/success.svg') ?>">
+			<img class="info hidden error" src="<?= $this->assetUrl('img/error.svg') ?>">
+		</form>
+		<div class="github-button">
+			<a class="github-button" href="https://github.com/n-peugnet/image-map-creator" data-show-count="true" aria-label="Star n-peugnet/image-map-creator on GitHub">Star</a>
+		</div>
+	</div>
 
 	<h2>Recently published maps:</h2>
 	<ul id="maps"></ul>
 
 	<script type="text/javascript">
-		let iMap = new imageMapCreator(700, 555);
+		let iMap = new imageMapCreator(695, 555);
 		let sketch = new p5(iMap.sketch.bind(iMap), "div-1");
 
 		// Globals for loading script
